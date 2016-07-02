41f41f7baa4906fc47b02f621ce1b07f451aa724
softprops <d.tangren@gmail.com>
2016-07-02 15:34:55 -0400

    invalid type test

--- serde-tests/test.rs
+++ serde-tests/test.rs
@@ -35,3 +35,16 @@ fn fails_with_missing_value() {
         Err(e) => assert_eq!(e, envy::Error::MissingValue("doom"))
     }
 }
+
+#[test]
+fn fails_with_invalid_type() {
+    let data = vec![
+        (String::from("BAR"), String::from("test")),
+        (String::from("BAZ"), String::from("notabool")),
+        (String::from("DOOM"), String::from("1,2,3"))
+    ];
+    match envy::from_iter::<_, Foo>(data.into_iter()) {
+        Ok(_) => panic!("expected failure"),
+        Err(e) => assert_eq!(e, envy::Error::Custom(String::from("Invalid type. Expected `Bool`")))
+    }
+}
