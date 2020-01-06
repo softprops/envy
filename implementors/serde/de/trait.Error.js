d5dc05c506f53a079b2201fc51f4731a7b1686e6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-01-06 00:34:13 +0000

    Deploy softprops/envy to github.com/softprops/envy.git:gh-pages

---
+++ implementors/serde/de/trait.Error.js
@@ -0,0 +1,9 @@
+(function() {var implementors = {};
+implementors["envy"] = [{text:"impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.104/serde/de/trait.Error.html\" title=\"trait serde::de::Error\">Error</a> for <a class=\"enum\" href=\"envy/enum.Error.html\" title=\"enum envy::Error\">Error</a>",synthetic:false,types:["envy::error::Error"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
---
+++ source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["envy"] = {"name":"","files":["error.rs","lib.rs"]};
+createSourceSidebar();
