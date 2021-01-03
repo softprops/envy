adda4f6f06990731543dba76aac24925d5b7a2c8
softprops <softprops@users.noreply.github.com>
2021-01-03 06:03:23 +0000

    Automated deployment: Sun Jan  3 06:03:23 UTC 2021 a04b9ff89cbe8c5144f5e4fe6c0878d4fe65fcb0

--- implementors/core/marker/trait.Freeze.js
+++ implementors/core/marker/trait.Freeze.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["envy"] = [{"text":"impl&lt;'a&gt; Freeze for <a class=\"struct\" href=\"envy/struct.Prefixed.html\" title=\"struct envy::Prefixed\">Prefixed</a>&lt;'a&gt;","synthetic":true,"types":["envy::Prefixed"]},{"text":"impl Freeze for <a class=\"enum\" href=\"envy/enum.Error.html\" title=\"enum envy::Error\">Error</a>","synthetic":true,"types":["envy::error::Error"]}];
+implementors["envy"] = [{"text":"impl&lt;'a&gt; Freeze for Prefixed&lt;'a&gt;","synthetic":true,"types":[]},{"text":"impl Freeze for Error","synthetic":true,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- implementors/serde/de/trait.Error.js
+++ implementors/serde/de/trait.Error.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["envy"] = [{"text":"impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.106/serde/de/trait.Error.html\" title=\"trait serde::de::Error\">Error</a> for <a class=\"enum\" href=\"envy/enum.Error.html\" title=\"enum envy::Error\">Error</a>","synthetic":false,"types":["envy::error::Error"]}];
+implementors["envy"] = [{"text":"impl Error for Error","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
