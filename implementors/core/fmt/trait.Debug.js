968308ab90abfbb8aa3b2406d2f3e6d8687218f5
softprops <softprops@users.noreply.github.com>
2021-05-15 05:19:28 +0000

    deploy: 433fcd5bdeffcaa9f5517bd576290357541617d1

--- implementors/core/fmt/trait.Debug.js
+++ implementors/core/fmt/trait.Debug.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["envy"] = [{"text":"impl Debug for Error","synthetic":false,"types":[]}];
+implementors["envy"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"enum\" href=\"envy/enum.Error.html\" title=\"enum envy::Error\">Error</a>","synthetic":false,"types":["envy::error::Error"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
