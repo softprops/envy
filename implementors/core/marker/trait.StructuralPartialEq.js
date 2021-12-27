329d932d3f41d94b0e7eb6e4e63da8349d3183e1
softprops <softprops@users.noreply.github.com>
2021-12-27 20:44:52 +0000

    deploy: 801d81e7c3e443470e110bf4e34460acba113476

--- implementors/core/marker/trait.StructuralPartialEq.js
+++ implementors/core/marker/trait.StructuralPartialEq.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["envy"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.StructuralPartialEq.html\" title=\"trait core::marker::StructuralPartialEq\">StructuralPartialEq</a> for <a class=\"enum\" href=\"envy/enum.Error.html\" title=\"enum envy::Error\">Error</a>","synthetic":false,"types":["envy::error::Error"]}];
+implementors["envy"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.57.0/core/marker/trait.StructuralPartialEq.html\" title=\"trait core::marker::StructuralPartialEq\">StructuralPartialEq</a> for <a class=\"enum\" href=\"envy/enum.Error.html\" title=\"enum envy::Error\">Error</a>","synthetic":false,"types":["envy::error::Error"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
